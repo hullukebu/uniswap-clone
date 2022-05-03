de3fd5ee099ec0aa675247f84042d10606d76675
donhamalainen <hamalainen.don@gmail.com>
2022-05-03 13:24:35 +0300

    Update

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Header from '../components/Header'
+
+const style = {
+  wrapper: `h-screen max-h-screen h-min-screen w-screen bg-[#2D242F] text-white select-none flex flex-col justify-between`,
+}
+
+export default function Home() {
+  return (
+    <div className={style.wrapper}>
+      <Header />
+      <h2>Hello</h2>
+      <h2>Hello</h2>
+    </div>
+  )
+}
